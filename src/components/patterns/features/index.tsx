2e17ff753e47e87e50d1669cc91ac149c2fe56eb
Philippe <akaphilippe@gmail.com>
2021-02-08 21:24:00 -0500

    add features stories

--- src/components/patterns/features/index.tsx
+++ src/components/patterns/features/index.tsx
@@ -7,18 +7,19 @@ interface IProps {
   data: IFeature[]
 }
 
-const Features: React.FC<IProps> = ({ data }) => {
+export const Features: React.FC<IProps> = ({ data }) => {
   const displayFeatures =
     data !== undefined
       ? data.map((feature: IFeature, index: number) => {
           return (
             <Feature
-              key={feature.id}
+              key={feature?.id}
               caption={feature.caption}
               featureImage={feature.featureImage}
               headline={feature.headline}
               content={feature.content}
               reverseGridItemsOrder={!isEven(index)}
+              link={feature?.link}
             />
           )
         })
