76b5430975000916bafec53eb962ea51ab351789
Philippe <akaphilippe@gmail.com>
2021-02-11 08:48:31 -0500

    update column interface

--- src/components/layout/Column/index.tsx
+++ src/components/layout/Column/index.tsx
@@ -1,9 +1,38 @@
 import React from "react"
-import { Box } from "@chakra-ui/react"
-import { IColumn } from "@src/@interfaces"
+import { Box, BoxProps } from "@chakra-ui/react"
+
 import { colSizes } from "./colSizes"
 
-export const Column: React.FC<IColumn> = ({ children, col, ...props }) => {
+type SmallDeviceColumnSizes = "sm1" | "sm2" | "sm3" | "sm4"
+
+type MediumDeviceColumnSizes = "md1" | "md2" | "md3" | "md4" | "md5" | "md6"
+
+type LargeDeviceColumnSizes =
+  | "lg1"
+  | "lg2"
+  | "lg3"
+  | "lg4"
+  | "lg5"
+  | "lg6"
+  | "lg7"
+  | "lg8"
+  | "lg9"
+  | "lg10"
+  | "lg11"
+  | "lg12"
+
+export interface ColumnProps extends BoxProps {
+  /**
+   * define the width of the column
+   */
+  col?: (
+    | LargeDeviceColumnSizes
+    | MediumDeviceColumnSizes
+    | SmallDeviceColumnSizes
+  )[]
+}
+
+export const Column: React.FC<ColumnProps> = ({ children, col, ...props }) => {
   // in case the prop col is not provided by developer
   // display the column with 100%
 
