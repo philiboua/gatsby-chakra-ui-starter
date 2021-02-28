618cd2653bfed3dcb84d5821bfa9bb1f3954e34c
Philippe <akaphilippe@gmail.com>
2021-02-28 14:19:57 -0500

    remove commented code

--- src/@types/assets/index.d.ts
+++ src/@types/assets/index.d.ts
@@ -1,10 +1,3 @@
-// declare module "*.svg" {
-//   import React = require("react")
-
-//   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
-//   const src: string
-//   export default src
-// }
 declare module "*.jpg" {
   const content: string
   export default content
