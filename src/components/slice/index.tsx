880f86e625fe001cc0c954b1fbce584880aa4d55
Philippe <akaphilippe@gmail.com>
2021-03-03 21:12:11 -0500

    update slice component, add spec for children

--- src/components/slice/index.tsx
+++ src/components/slice/index.tsx
@@ -4,6 +4,7 @@ export type SlicesType = "body" | "media" | "header" | "footer" | "background"
 
 interface SliceProps {
   __TYPE: SlicesType
+  children: React.ReactNode
 }
 
 export const Slice: React.FC<SliceProps> = ({ children }) => {
