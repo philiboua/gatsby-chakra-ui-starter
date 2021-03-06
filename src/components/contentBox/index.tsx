a9c0a3d4578d4aa1ae2a14cca7405db7c51889c3
Philippe <akaphilippe@gmail.com>
2021-03-06 15:04:03 -0500

    update / add storybook stories

--- src/components/contentBox/index.tsx
+++ src/components/contentBox/index.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import { bgWithHightSaturation } from "@src/contexts"
 
 interface ContentBoxProps {
+  /**
+   * Provide indication about the color saturation of the background
+   * In case the background is highly saturated, the text color will be white
+   */
   hightSaturatedBg?: boolean
   children: React.ReactNode
 }
