8b0e2af23183eef19499e07199cc58352fa64d63
Philippe <akaphilippe@gmail.com>
2021-04-08 07:03:23 -0400

    add number input field with tests and stories

---
+++ src/components/form/components/numberInput/tests/NumberInputForm.test.tsx
@@ -0,0 +1,101 @@
+import React from "react"
+import { screen, render, testA11y, waitFor } from "@src/tests"
+import userEvent from "@testing-library/user-event"
+import { NumberForm } from "./mock/NumberForm"
+
+/**
+ * We are testing the Input component
+ * Below you can find details of the component  :
+ *
+ *  <NumberInput name="age" label="Enter your age" />
+ *
+ */
+
+describe("Number Input field : <NumberInput/>", () => {
+  it("passes a11y test", async () => {
+    const handleSubmit = jest.fn()
+    await testA11y(<NumberForm submitHandler={handleSubmit} />)
+  })
+
+  describe("initial state", () => {
+    it("renders a number field", () => {
+      const handleSubmit = jest.fn()
+      render(<NumberForm submitHandler={handleSubmit} />)
+
+      expect(screen.getByLabelText("Enter your age")).toBeInTheDocument()
+      expect(screen.getByLabelText("Enter your age").nodeValue).toEqual(null)
+    })
+
+    it("renders no error message", () => {
+      const handleSubmit = jest.fn()
+      render(<NumberForm submitHandler={handleSubmit} />)
+
+      expect(screen.queryByText("age is a required field")).toBe(null)
+    })
+  })
+
+  describe("user interactions", () => {
+    describe("user submits the form with valid data", () => {
+      it("renders no error message", async () => {
+        const submitFn = jest.fn()
+        render(<NumberForm submitHandler={submitFn} />)
+
+        const input = screen.getByLabelText("Enter your age")
+        userEvent.type(input, "18")
+
+        const submitBtn = screen.getByRole("button", { name: /submit/i })
+        userEvent.click(submitBtn)
+
+        await waitFor(() => {
+          expect(submitFn).toHaveBeenCalledTimes(1)
+          expect(submitFn).toHaveBeenCalledWith({ age: "18" })
+        })
+      })
+    })
+
+    describe("user submits the form with invalid data", () => {
+      it("renders an error message", async () => {
+        const submitFn = jest.fn()
+        render(<NumberForm submitHandler={submitFn} />)
+
+        const submitBtn = screen.getByRole("button", { name: /submit/i })
+        userEvent.click(submitBtn)
+
+        await waitFor(() => {
+          expect(submitFn).toHaveBeenCalledTimes(0)
+          expect(screen.queryByText("age is a required field")).not.toBe(null)
+        })
+      })
+    })
+
+    describe("user fills the input field with valid data and go to another element by tabing", () => {
+      it("renders no error message", async () => {
+        const submitFn = jest.fn()
+        render(<NumberForm submitHandler={submitFn} />)
+
+        const input = screen.getByLabelText("Enter your age")
+        userEvent.type(input, "18")
+        userEvent.tab()
+
+        await waitFor(() => {
+          expect(screen.queryByText("age is a required field")).toBe(null)
+        })
+      })
+    })
+
+    describe("user fills the input field with invalid data and go to another element by tabing", () => {
+      it("renders an error message", async () => {
+        const submitFn = jest.fn()
+        render(<NumberForm submitHandler={submitFn} />)
+
+        const input = screen.getByLabelText("Enter your age")
+        userEvent.type(input, "")
+        userEvent.tab()
+
+        await waitFor(() => {
+          expect(screen.queryByText("age is a required field")).not.toBe(null)
+        })
+      })
+    })
+  })
+})
---
+++ src/components/form/components/numberInput/tests/mock/NumberForm.tsx
@@ -0,0 +1,53 @@
+import React from "react"
+import { FormControl, NumberInput, SubmitButton } from "@src/components"
+import { chakra } from "@chakra-ui/react"
+import { Formik } from "formik"
+import * as Yup from "yup"
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+
+interface IntialValues {
+  age: string
+}
+
+const initialValues: IntialValues = {
+  age: "",
+}
+
+const validationSchema = Yup.object({
+  age: Yup.number().required(),
+})
+
+export const NumberForm: React.FC<{
+  submitHandler: (v: unknown) => void
+}> = ({ submitHandler }) => {
+  const handleOnSubmit = async (values: unknown) => {
+    await sleep(500)
+    submitHandler(values)
+  }
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleOnSubmit}
+    >
+      {({ handleSubmit }) => (
+        <chakra.form
+          borderWidth="1px"
+          rounded="lg"
+          shadow="1px 1px 3px rgba(0,0,0,0.3)"
+          maxWidth={800}
+          p={6}
+          m="10px auto"
+          onSubmit={handleSubmit}
+        >
+          <FormControl name="age">
+            <NumberInput name="age" label="Enter your age" />
+          </FormControl>
+          <SubmitButton>Submit</SubmitButton>
+        </chakra.form>
+      )}
+    </Formik>
+  )
+}
